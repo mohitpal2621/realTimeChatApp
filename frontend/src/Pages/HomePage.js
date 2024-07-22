15d4b73d7d1982cd49a1067b64ae3b495740ff40
Mohit Pal <mohitpal2621@gmail.com>
2024-07-22 23:45:13 +0530

    Login & Signup

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,7 +1,60 @@
 import React from "react";
+import Login from "../component/Authentication/Login";
+import Signup from "../component/Authentication/Signup";
+import {
+	Box,
+	Container,
+	Text,
+	Tabs,
+	Tab,
+	TabList,
+	TabPanels,
+	TabPanel,
+} from "@chakra-ui/react";
 
 const HomePage = () => {
-	return <div>Home</div>;
+	return (
+		<Container maxW="xl" centerContent>
+			<Box
+				display="flex"
+				alignItems="center"
+				justifyContent="center"
+				p={3}
+				bg={"white"}
+				w="100%"
+				m="40px 0 15px 0"
+				borderRadius="lg"
+				borderWidth="1px"
+			>
+				<Text fontSize="4xl" fontFamily="Work sans" color="black">
+					Chat-Pulse
+				</Text>
+			</Box>
+			<Box
+				bg="white"
+				w="100%"
+				p={4}
+				color="black"
+				borderRadius="lg"
+				borderWidth="1px"
+			>
+				<Tabs variant="soft-rounded">
+					<TabList mb="1em">
+						<Tab width="50%">Login</Tab>
+						<Tab width="50%">Sign up</Tab>
+					</TabList>
+					<TabPanels>
+						<TabPanel>
+							<Login />
+						</TabPanel>
+						<TabPanel>
+							<Signup />
+						</TabPanel>
+					</TabPanels>
+				</Tabs>
+			</Box>
+		</Container>
+	);
 };
 
 export default HomePage;
---
+++ frontend/src/component/Authentication/Signup.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import {
+	Button,
+	FormControl,
+	FormLabel,
+	Input,
+	InputGroup,
+	InputRightElement,
+	VStack,
+} from "@chakra-ui/react";
+
+const Signup = () => {
+	const [show, setShow] = useState(false);
+	const [name, setName] = useState();
+	const [email, setEmail] = useState();
+	const [password, setPassword] = useState();
+	const [confirmPassword, setConfirmPassword] = useState();
+	const [pic, setPic] = useState();
+
+	function handleClick() {
+		setShow((prev) => !prev);
+	}
+
+	function postDetails(pics) {}
+
+	function submitHandler() {}
+
+	return (
+		<VStack spacing="5px">
+			<FormControl id="first-name" isRequired>
+				<FormLabel>Name</FormLabel>
+				<Input
+					placeholder="Enter your name"
+					onChange={(e) => setName(e.target.value)}
+				/>
+			</FormControl>
+			<FormControl id="email" isRequired>
+				<FormLabel>Email Address</FormLabel>
+				<Input
+					type="email"
+					placeholder="Enter email-id"
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+			</FormControl>
+			<FormControl id="password" isRequired>
+				<FormLabel>Password</FormLabel>
+				<InputGroup size="md">
+					<Input
+						type={show ? "text" : "password"}
+						placeholder="Enter password"
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<InputRightElement width="4.5rem">
+						<Button h="1.75rem" size="sm" onClick={handleClick}>
+							{show ? "Hide" : "Show"}
+						</Button>
+					</InputRightElement>
+				</InputGroup>
+			</FormControl>
+			<FormControl id="confirm-password" isRequired>
+				<FormLabel>Confirm Password</FormLabel>
+				<InputGroup size="md">
+					<Input
+						type={show ? "text" : "password"}
+						placeholder="Confirm password"
+						onChange={(e) => setConfirmPassword(e.target.value)}
+					/>
+					<InputRightElement width="4.5rem">
+						<Button h="1.75rem" size="sm" onClick={handleClick}>
+							{show ? "Hide" : "Show"}
+						</Button>
+					</InputRightElement>
+				</InputGroup>
+			</FormControl>
+			<FormControl id="pic">
+				<FormLabel>Upload your picture</FormLabel>
+				<Input
+					type="file"
+					p={1.5}
+					accept="image/*"
+					onChange={(e) => postDetails(e.target.files[0])}
+				/>
+			</FormControl>
+			<Button
+				colorScheme="blue"
+				width="100%"
+				style={{ marginTop: 15 }}
+				onClick={submitHandler}
+			>
+				Sign up
+			</Button>
+		</VStack>
+	);
+};
+
+export default Signup;
